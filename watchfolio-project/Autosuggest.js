15bfeeaf4b6580e3b0d3f86c362fae30a6b8b42c
JoshuaPoon1 <93095025+JoshuaPoon1@users.noreply.github.com>
2023-04-12 17:06:29 -0400

    Create Autosuggest.js
    Include on main scripts. ---to be deleted---

---
+++ watchfolio-project/Autosuggest.js
@@ -0,0 +1,35 @@
+const searchBar = document.querySelector(".searchbar")
+const suggestion = document.querySelector(".suggestion"); //this is an ul
+
+const watchNames = [
+    "Rolex Submariner",
+    "Rolex Daytona",
+    "Rolex GMT-Master II",
+    "Rolex Datejust",
+    "Omega Speedmaster Professional",
+    "Omega Seamaster Diver 300M",
+    "Omega Constellation",
+    "Omega De Ville Prestige",
+    "TAG Heuer Carrera",
+    "Breitling Navitimer",
+    "IWC Portugieser",
+    "Jaeger-LeCoultre Reverso",
+    "Patek Philippe Nautilus",
+    "Audemars Piguet Royal Oak",
+    "Cartier Tank",
+    "Panerai Luminor"
+  ];
+
+searchBar.addEventListener("keyup",()=>{
+    clear();
+    watchNames.forEach((watch)=>{
+        if (watch.toLowerCase().startsWith(searchBar.value.toLowerCase()) && searchBar.value != ""){
+            let html = `<li><b>${watch.substring(0,searchBar.value.length)}</b>${watch.substring(searchBar.value.length,watch.length)}</li>`
+            suggestion.insertAdjacentHTML("beforeend",html)
+        }
+    })
+})
+
+function clear(){
+    suggestion.innerHTML = "";
+}
