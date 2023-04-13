e3bfd97d569130106d89106c0f0d3e6707e45453
JoshuaPoon1 <93095025+JoshuaPoon1@users.noreply.github.com>
2023-04-13 14:06:10 -0400

    Update script.js

--- watchfolio-project/script.js
+++ watchfolio-project/script.js
@@ -1,51 +1,11 @@
-/*for dark mode, handling modal clicks, and automating sliders. ~五百二十赫兹 */
-
-const darkMode = document.querySelector(".moon-svg");
-const headerText = document.querySelectorAll(".header-text");
-const sectionElement = document.querySelector("section");
-const listElement = document.querySelectorAll("li");
-const parentSlider = document.querySelectorAll(".parent-slider");
-const sliderTitle = document.querySelectorAll(".slider-title");
-const more = document.querySelectorAll(".more");
-const guideP = document.querySelectorAll(".guide p");
-const sort = document.querySelector(".sort");
+const trendingSliders = document.querySelectorAll(".slider.trending");
+const trendingDots = document.querySelectorAll(".dot.trending");
+const gainerSliders = document.querySelectorAll(".slider.gainers");
+const gainerDots = document.querySelectorAll(".dot.gainer");
 const blur = document.querySelector(".overlay-blur");
 const modules = document.querySelectorAll(".module");
 const links = document.querySelectorAll(".wf-link");
 const signup = document.querySelector(".sign-up");
-
-
-darkMode.addEventListener("click", darkModeToggle);
-
-function darkModeToggle(){
-  document.body.classList.toggle("background-color-black");
-  headerText.forEach((a) => {
-    a.classList.toggle("color-white");
-  });
-  sectionElement.classList.toggle("background-color-black");
-  parentSlider.forEach((slider) => {
-    slider.classList.toggle("bkgrnd-prpl");
-  });
-  listElement.forEach((el) => {
-    el.classList.toggle("color-white");
-  });
-  sliderTitle.forEach((style) => {
-    style.classList.toggle("color-green");
-  });
-  more.forEach((el) => {
-    el.classList.toggle("color-white");
-  });
-
-  guideP.forEach((p) => p.classList.toggle("color-white"));
-  sort.classList.toggle("color-white");
-};
-
-darkModeToggle();
-
-const trendingSliders = document.querySelectorAll(".slider.trending");
-const trendingDots = document.querySelectorAll(".dot.trending")
-const gainerSliders = document.querySelectorAll(".slider.gainers");
-const gainerDots = document.querySelectorAll(".dot.gainer")
 
 const dots = document.querySelectorAll(".dot");
 
@@ -55,16 +15,16 @@ const goToSlide = (slideNo, specificSlider) => {
   });
 };
 
-const activeDot = (slideNo,specificDots) =>{
+const activeDot = (slideNo, specificDots) => {
   specificDots.forEach((dot) => {
     dot.classList.remove("active");
   });
   //const activeDot = document.querySelector(`.dot[data-slide="${slideNo}"]`);
-  const activeDot = specificDots[slideNo]
+  const activeDot = specificDots[slideNo];
   activeDot?.classList.add("active");
 };
 
-const nextSlide = (seconds, specificSlider,specificDots) => {
+const nextSlide = (seconds, specificSlider, specificDots) => {
   let currentSlide = 0;
   setTimeout(() => {
     setInterval(function () {
@@ -74,14 +34,13 @@ const nextSlide = (seconds, specificSlider,specificDots) => {
         currentSlide++;
       }
       goToSlide(currentSlide, specificSlider);
-      activeDot(currentSlide,specificDots);
+      activeDot(currentSlide, specificDots);
     }, 3000);
   }, seconds * 1000);
 };
 
 nextSlide(0, trendingSliders, trendingDots);
 nextSlide(1.5, gainerSliders, gainerDots);
-
 
 const show = (str) => {
   blur.classList.remove("hidden");
